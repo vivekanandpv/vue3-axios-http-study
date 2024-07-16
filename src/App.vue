1f013224995e0ad7d6d1abbd1e4d710b6cf9454e
Vivekanand P V <vivek@anviksha.io>
2024-07-16 14:29:18 +0530

    done

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,18 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
+import { reqresInstance } from './services/http.client.ts';
+
+onMounted(async () => {
+  try {
+    //  since the http client is configured as the global cofiguration (as a plugin),
+    //  we can use it as part of every child of the app
+    //  More info: https://github.com/axios/axios#axios-api
+    const data = await reqresInstance.get(`users?page=2`);
+    console.log("Server", data);
+  } catch (error) {
+    console.log("Error", error);
+  }
+});
 </script>
 
 <template>
