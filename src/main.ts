cf8872bc36ffafe78903e4666e154092601cf353
Vivekanand P V <vivek@anviksha.io>
2024-07-16 14:21:47 +0530

    done

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import { reqresInstance, typicodeInstance } from './services/http.client';
 
-createApp(App).mount('#app');
+createApp(App)
+  .use({
+    install(vueApp) {
+      vueApp.config.globalProperties.$reqresService = reqresInstance;
+      vueApp.config.globalProperties.$typicodeService = typicodeInstance;
+    },
+  })
+  .mount('#app');
